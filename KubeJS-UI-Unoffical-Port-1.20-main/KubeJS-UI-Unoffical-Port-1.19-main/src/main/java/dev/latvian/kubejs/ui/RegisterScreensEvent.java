8827810a76ce159cf49e597ffab8ebabd2a69979
Blue Lotus Coding <62197849+DeveloperNoShinigami@users.noreply.github.com>
2025-07-03 11:43:50 -0500

    Delete KubeJS-UI-Unoffical-Port-1.20-main/KubeJS-UI-Unoffical-Port-1.19-main directory

--- KubeJS-UI-Unoffical-Port-1.20-main/KubeJS-UI-Unoffical-Port-1.19-main/src/main/java/dev/latvian/kubejs/ui/RegisterScreensEvent.java
+++
@@ -1,8 +0,0 @@
-package dev.latvian.kubejs.ui;
-
-import java.util.function.BiConsumer;
-
-@FunctionalInterface
-public interface RegisterScreensEvent {
-	void registerScreens(BiConsumer<Class<?>, String> registry);
-}
